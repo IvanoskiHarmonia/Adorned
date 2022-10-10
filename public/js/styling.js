0517b441a7caf4e7bb1abb2d8430aa90e991d21f
Martin Ivanoski <martinivanoski@Martins-MBP.lan>
2022-10-10 08:57:10 -0500

    Design Changes 2.0 - finished landing page.

--- public/js/styling.js
+++ public/js/styling.js
@@ -1,13 +1,42 @@
+window.onload = () => {
+    addNavStyle();
+}
+
+
 
 const navbar = document.querySelector('.navbar');
 
-
+var prevScrollpos = window.pageYOffset;
 
 window.onscroll = () => {
-    if (window.scrollY > 80) {
-        navbar.classList.add('navbar-style');
-    } else { 
-        navbar.classList.remove('navbar-style');
-    }
-// console.log(window.pageYOffset);
+    hideNav();
+    addNavStyle();
 };
+
+
+
+
+
+
+
+
+// Navbar scroll effects
+
+// hideNav
+const hideNav = () => {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        prevScrollpos = currentScrollPos;
+        navbar.style.top = "0";
+    }
+     else {
+        navbar.style.top = "-80px";
+        prevScrollpos = currentScrollPos;
+    }
+}
+
+// addNavStyle
+const addNavStyle = () => {
+    if (window.scrollY > 80) navbar.classList.add('navbar-style');
+    else  navbar.classList.remove('navbar-style');
+}
